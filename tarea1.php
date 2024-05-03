d3e8a2ae30e6712556e7c58fcc1d0171b884861b
“Castellanos“ <rcastellanos11383@gmail.com>
2024-05-03 10:04:53 -0600

    No. 2

---
+++ tarea1.php
@@ -0,0 +1,36 @@
+<?php
+
+class Producto {
+
+    protected string $nombre;
+    public int $precio;
+    public bool $disponible;
+    public function __construct(string $nombre, int $precio, bool $disponible) 
+    
+    {
+
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponible = $disponible;
+    }
+
+    public function mostrarProducto(){
+        echo "El Productop es: " . $this->nombre ." y su precio es de: ". $this->precio ."";
+    }
+
+    public function getNombre() : string{
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre){
+        $this-> nombre = $nombre;
+    }
+
+}
+
+$producto = new Producto('Tablet', 200, true);
+
+// echo $producto->nombre;
+echo $producto->getNombre();
+$producto ->setNombre (' Nuevo Nombre ');
+echo $producto->getNombre();
